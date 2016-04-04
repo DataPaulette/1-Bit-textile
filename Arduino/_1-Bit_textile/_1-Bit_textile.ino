0661f585b029c939663452ec0efd42d231e3477c
alice <alice@datapaulette.org>
2016-04-04 19:42:39 +0200

    tests 3 bobines

--- Arduino/_1-Bit_textile/_1-Bit_textile.ino
+++ Arduino/_1-Bit_textile/_1-Bit_textile.ino
@@ -1,27 +1,50 @@
+const int LEDPIN = 13;
 
-const int IOA = 8;
-const int IOB = 9;
+const int IOA1 = 8;
+const int IOB1 = 9;
+const int IOA2 = 12;
+const int IOB2 = 11;
+const int IOA3 = 7;
+const int IOB3 = 6;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode (IOA, OUTPUT);
-  pinMode (IOB, OUTPUT);
 
+  Serial.begin(9600);
+  pinMode (IOA1, OUTPUT);
+  pinMode (IOB1, OUTPUT);
+  pinMode (IOA2, OUTPUT);
+  pinMode (IOB2, OUTPUT);
+  pinMode (IOA3, OUTPUT);
+  pinMode (IOB3, OUTPUT);
+  pinMode (LEDPIN, OUTPUT);
 }
 
 void loop() {
 
-
-  turnLeft( 1000, 1 );
+  turnLeft( IOA1, IOB1, 5000, 10 );
   Serial.println("Left");
+  digitalWrite(LEDPIN, HIGH);
+  turnLeft( IOA2, IOB2, 5000, 10 );
+  Serial.println("Left");
+  digitalWrite(LEDPIN, HIGH);
+  turnLeft( IOA3, IOB3, 5000, 10 );
+  Serial.println("Left");
+  digitalWrite(LEDPIN, HIGH);
   delay(1000);
-  turnRight( 1000, 1 );
+  turnRight( IOA1, IOB1, 5000, 10 );
   Serial.println("Right");
+  digitalWrite(LEDPIN, LOW);
+  turnRight( IOA2, IOB2, 5000, 10 );
+  Serial.println("Right");
+  digitalWrite(LEDPIN, LOW);
+  turnRight( IOA3, IOB3, 5000, 10 );
+  Serial.println("Right");
+  digitalWrite(LEDPIN, LOW);
   delay(1000);
 
 }
 
-void turnLeft( int pulse, int del ) {
+void turnLeft( const int IOA, const int IOB, int pulse, int del ) {
   for (int i = 0; i < pulse; i++) {
     digitalWrite (IOA, LOW);
     digitalWrite (IOB, HIGH);
@@ -32,7 +55,7 @@ void turnLeft( int pulse, int del ) {
   }
 }
 
-void turnRight( int pulse, int del ) {
+void turnRight( const int IOA, const int IOB, int pulse, int del ) {
   for (int i = 0; i < pulse; i++) {
     digitalWrite (IOA, HIGH);
     digitalWrite (IOB, LOW);
